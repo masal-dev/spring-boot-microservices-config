44aa8078e97e48a658d27b3c70ac5cb230f95086
Marco Salernitano <masal.weblogins@gmail.com>
2024-09-11 22:40:19 +0200

    Added Greeting controller
    Example for getting greeting message from application.properties file
    End point: /greeting

---
+++ src/main/java/it/salernitano/spring_boot_microservices_config/SpringBootMicroservicesConfigApplication.java
@@ -0,0 +1,13 @@
+package it.salernitano.spring_boot_microservices_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/salernitano/spring_boot_microservices_config/SpringBootMicroservicesConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package it.salernitano.spring_boot_microservices_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
